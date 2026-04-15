99c6ed9ae214e4aa463ac03b3d3898e77f2c6116
Leticia <leticiasuelenc@gmail.com>
2026-04-15 22:54:28 +0000

    Adicionar componente Header com slogan

---
+++ lineare-vite/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="logo">Lineare</h1>
+        <p className="tagline">Sua rotina em linha reta.</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
